b4f797e1eb95a22ccebeffb2bc0e42137f52be8e
Shiou-Yan <black00731@gmail.com>
2020-08-30 13:46:07 +0800

    + phone & address to user table

---
+++ database/migrations/2020_08_30_041339_add_to_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->smallInteger('phone')->after('password');
+            $table->text('address')->after('phone');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('phone');
+            $table->dropColumn('address');
+        });
+    }
+}
