9a7b6d60fadef9f559a56650daebb92ae58023ae
Shiou-Yan <black00731@gmail.com>
2020-08-30 13:50:27 +0800

    + commodities table & Model & Controller

---
+++ app/Commodity.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Commodity extends Model
+{
+
+    protected $fillable = [
+        'name', 'price', 'picture',
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo('App/Category');
+    }
+}
